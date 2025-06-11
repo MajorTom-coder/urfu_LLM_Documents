8ee468ea92afdbacf21849fb502be5c2b6d6a271
Nesmelov Pavel <nesmelovpavel2@gmail.com>
2025-06-11 10:07:16 +0500

    Осталось добить эмбединги

--- lama 3.1/proverka8/DocumentChecker.py
+++ lama 3.1/proverka8/DocumentChecker.py
@@ -3,23 +3,40 @@ import pytesseract
 import os
 from cv2.gapi import kernel
 from pdf2image import convert_from_path
+from sentence_transformers import SentenceTransformer, util
 from sklearn.cluster import DBSCAN
 from pathlib import Path
 
 def local_to_absolute_path(file_path):
     return str(Path(file_path).resolve())
 
-"""
 # Загрузка модели эмбеддингов
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
 # Сравнение с использованием эмбеддингов напрямую
-def compare_names_with_embeddings(name1, name2, threshold=0.8):
-    if name1 and name2:
-        name1_clean = preprocess_text(name1)
-        name2_clean = preprocess_text(name2)
-        emb1 = model.encode(name1_clean, convert_to_tensor=True)
-        emb2 = model.encode(name2_clean, convert_to_tensor=True)
+def compare_names_with_embeddings(threshold=0.8):
+    pytesseract.pytesseract.tesseract_cmd = local_to_absolute_path('Tesseract/tesseract.exe')
+
+    approve_arr = [[len(os.listdir(local_to_absolute_path('data/change_image')))],[]]
+    agreed_arr = [[len(os.listdir(local_to_absolute_path('data/change_image')))],[]]
+
+    count = 0
+    for value in os.listdir(local_to_absolute_path('data/change_image')):
+        img = cv2.imread(value)
+        data = pytesseract.image_to_data(img, lang='rus', output_type=pytesseract.Output.DICT)
+
+        for iterator in range(len(data)):
+            if 'утверждаю' in data['text'][iterator].lower().strip() or 'утверждено' in data['text'][iterator].lower().strip():
+                approve_arr[count] = data['text']
+            elif 'согласов' in data['text'][iterator].lower().strip():
+                agreed_arr[count] = data['text']
+
+        count += 1
+
+    """
+    if approve and agreed:
+        emb1 = model.encode(approve, convert_to_tensor=True)
+        emb2 = model.encode(agreed, convert_to_tensor=True)
         similarity = util.pytorch_cos_sim(emb1, emb2).item()
 
         if similarity >= threshold:
@@ -28,11 +45,12 @@ def compare_names_with_embeddings(name1, name2, threshold=0.8):
             return f"Наименования не совпадают (сходство: {similarity:.2f})"
     else:
         return "Ошибка: Не удалось извлечь наименования"
-"""
+    """
 
 #Преобразование pdf к jpg
-def convertPDFToImage(file_path, save_image_path = local_to_absolute_path('data/pdf_images'), poppler_path = local_to_absolute_path('urfu_LLM_Documents/lama 3.1/poppler-24.08.0/Library/bin')):
-    deleteFileInFolder()
+def convert_PDF_to_image(file_path, save_image_path = local_to_absolute_path('data/pdf_images'), poppler_path = local_to_absolute_path('urfu_LLM_Documents/lama 3.1/poppler-24.08.0/Library/bin')):
+    print('Шаг 1 Конвертация pdf к изображению...')
+    delete_file_in_folder()
     os.environ["PATH"] += os.pathsep + poppler_path
     images = convert_from_path(file_path)
 
@@ -40,7 +58,7 @@ def convertPDFToImage(file_path, save_image_path = local_to_absolute_path('data/
         image.save(f'{save_image_path}\\page_{i}.jpg', 'JPEG')
 
 #Удаление предыдущих результатов преобразования pdf к jpg
-def deleteFileInFolder(file_path = local_to_absolute_path('data/pdf_images')):
+def delete_file_in_folder(file_path = local_to_absolute_path('data/pdf_images')):
     for filename in os.listdir(file_path):
         file_path = os.path.join(file_path, filename)
         try:
@@ -50,8 +68,11 @@ def deleteFileInFolder(file_path = local_to_absolute_path('data/pdf_images')):
             print(f'Ошибка при удалении файла {file_path}. {e}')
 
 #Анализ страницы
-def analyzeImages(images_path = local_to_absolute_path('data/pdf_images')):
+def get_text_blocks(images_path = local_to_absolute_path('data/pdf_images')):
+    print('Шаг 2 Подготовка к обработке изображения')
     pytesseract.pytesseract.tesseract_cmd = local_to_absolute_path('Tesseract/tesseract.exe')
+
+    page_counter = 0
 
     #Открываем контекст одной страницы
     for i in range(1, len(os.listdir(images_path)) + 1):
@@ -75,7 +96,7 @@ def analyzeImages(images_path = local_to_absolute_path('data/pdf_images')):
                 left_arr[0] = int(data['left'][iterator])
                 top_arr[0] = int(data['top'][iterator])
 
-            if 'согласовано' in data['text'][iterator].lower().strip():
+            if 'согласов' in data['text'][iterator].lower().strip():
                 flag_arr[1] = True
                 left_arr[1] = int(data['left'][iterator])
                 top_arr[1] = int(data['top'][iterator])
@@ -95,7 +116,7 @@ def analyzeImages(images_path = local_to_absolute_path('data/pdf_images')):
                     centers.append([cx, cy])
 
         if not centers:
-            print("Текст не найден.")
+            print("Обработка в процессе...")
             continue
 
         # Кластеризация по координатам центров
@@ -109,6 +130,8 @@ def analyzeImages(images_path = local_to_absolute_path('data/pdf_images')):
                 clusters[label] = []
             clusters[label].append(boxes[iterator])
 
+        cropped_image = img
+
         # Рисуем рамки вокруг абзацев
         for box_list in clusters.values():
             xs = [x for (x, y, w, h) in box_list]
@@ -119,36 +142,30 @@ def analyzeImages(images_path = local_to_absolute_path('data/pdf_images')):
             x_min, y_min = min(xs), min(ys)
             x_max, y_max = max(ws), max(hs)
 
-            cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (10, 255, 12), 2)
+            page_counter += 1
 
-        cv2.imwrite(f'{local_to_absolute_path('data/change_image')}\\page_{i}.jpg', img)
+            cropped_image = img[y_min - 50:y_max + 50, x_min - 50 :x_max + 50]
 
-        """
-        #Выделение блоков на изображении
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (7, 7), 0)
-        thresh = cv2.threshold(blur, 0, 255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-        kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 13))
-        dilate = cv2.dilate(thresh, kernal, iterations=1)
+            cropped_image_data = pytesseract.image_to_data(cropped_image, lang='rus', output_type=pytesseract.Output.DICT)
+
+            for iterator in range(len(cropped_image_data['text'])):
+                if 'утверждаю' in cropped_image_data['text'][iterator].lower().strip() or 'утверждено' in cropped_image_data['text'][iterator].lower().strip() or 'согласов' in cropped_image_data['text'][iterator].lower().strip():
+                    cv2.imwrite(f'{local_to_absolute_path('data/change_image')}\\cropped_image_{page_counter}.jpg', cropped_image)
+                    print(f'Добавлен файл {local_to_absolute_path('data/change_image')}\\cropped_image_{page_counter}.jpg')
 
 
-        cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        cnts = sorted(cnts, key=lambda x: cv2.boundingRect(x)[0])
-        for c in cnts:
-            (x, y, w, h) = cv2.boundingRect(c)
-            #if h > 100 and w > 200:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (36, 255, 12), 2)
-        cv2.imwrite(f'D:\\OlegDocAnalyze\\fork_urfu_LLM_DOC\\urfu_LLM_Documents\\lama 3.1\\proverka8\\data\\change_image\\page_{i}.jpg', img)
-        """
+
 
 # Основная логика
 def main(pdf_path):
-    convertPDFToImage(pdf_path)
-    analyzeImages()
-    return f"Images successfully converted from {pdf_path}"
+    convert_PDF_to_image(pdf_path)
+    print(f'Images successfully converted from {pdf_path}')
+    get_text_blocks()
+    print(f'Обработка завершена!')
+    #compare_names_with_embeddings()
+
 
 # Запуск
-pdf_path = 'D:\\OlegDocAnalyze\\fork_urfu_LLM_DOC\\urfu_LLM_Documents\\lama 3.1\\proverka8\\data\\СП_16_Магнит_отдел_Выборы_Гос_Дума_2016_оп_2_п_хр_.pdf'
+pdf_path = 'D:\\OlegDocAnalyze\\fork_urfu_LLM_DOC\\urfu_LLM_Documents\\lama 3.1\\proverka8\\data\\Карталы_Редакция_газеты_оп_4_пх_за_2021_год.pdf'
 
 main(pdf_path)
